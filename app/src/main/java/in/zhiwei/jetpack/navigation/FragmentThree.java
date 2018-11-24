f86f3f15626c7e42eef7aafe7e557342530e2e0d
志威 <1396188796@qq.com>
2018-11-24 20:42:01 +0800

    添加备注，navigation跳转fragmeng都是新建，google官方设计如此

--- app/src/main/java/in/zhiwei/jetpack/navigation/FragmentThree.java
+++ app/src/main/java/in/zhiwei/jetpack/navigation/FragmentThree.java
@@ -1,6 +1,7 @@
 package in.zhiwei.jetpack.navigation;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,15 +16,25 @@ import in.zhiwei.jetpack.R;
  * Date: 2018/11/5 0005,20:35.
  */
 public class FragmentThree extends Fragment {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.w("Fragment","FragmentThree onCreate");
+
+    }
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        Log.w("Fragment","FragmentThree onCreateView");
         return inflater.inflate(R.layout.fragment_three, container, false);
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        Log.w("Fragment","FragmentThree onViewCreate");
         //返回
         view.findViewById(R.id.btn_back2).setOnClickListener(v -> Navigation.findNavController(v).navigateUp());
         //跳 1
--- app/src/main/java/in/zhiwei/jetpack/navigation/FragmentTwo.java
+++ app/src/main/java/in/zhiwei/jetpack/navigation/FragmentTwo.java
@@ -1,6 +1,7 @@
 package in.zhiwei.jetpack.navigation;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,15 +16,23 @@ import in.zhiwei.jetpack.R;
  * Date: 2018/11/5 0005,20:35.
  */
 public class FragmentTwo extends Fragment {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.i("Fragment","FragmentTwo onCreate");
+    }
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        Log.i("Fragment","FragmentTwo onCreateView");
         return inflater.inflate(R.layout.fragment_two, container, false);
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        Log.i("Fragment","FragmentTwo onViewCreate");
         //返回
         view.findViewById(R.id.btn_back1).setOnClickListener(v -> Navigation.findNavController(v).navigateUp());
         //跳 3
