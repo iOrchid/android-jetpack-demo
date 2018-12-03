e401c4dbe03e328daa2572e2203e4f06f15a64fc
志威 <1396188796@qq.com>
2018-12-03 11:05:57 +0800

    navigation的简要注意说明

--- app/src/main/java/in/zhiwei/jetpack/navigation/NaviActivity.java
+++ app/src/main/java/in/zhiwei/jetpack/navigation/NaviActivity.java
@@ -7,6 +7,8 @@ import androidx.navigation.Navigation;
 import in.zhiwei.jetpack.R;
 
 /**
+ * todo 目前已知使用Navigation框架，配置navGraph需要AndroidStudio3.2以上版本，在settings--experimental--enable navigation editor
+ * todo 就能使用类似于写layout的形式，看navGraph的配置了。而且配置的fragment每次触发都是创建新的对象实例（返回不会新建），这个google官方已经知晓，切不修改，认为这个是符合新的设计理念规范的。
  * navigation框架的演示
  * Author: zhiwei.
  * Date: 2018/11/5 0005,17:49.
