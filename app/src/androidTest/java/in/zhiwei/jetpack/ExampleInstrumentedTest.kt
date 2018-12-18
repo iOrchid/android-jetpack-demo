00453361f9c6a793c7e342665333d839d782b173
志威 <1396188796@qq.com>
2018-12-18 14:35:13 +0800

    更新test的依赖库

--- app/src/androidTest/java/in/zhiwei/jetpack/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/in/zhiwei/jetpack/ExampleInstrumentedTest.kt
@@ -1,7 +1,8 @@
 package `in`.zhiwei.jetpack
 
-import androidx.test.InstrumentationRegistry
-import androidx.test.runner.AndroidJUnit4
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
 import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -11,12 +12,12 @@ import org.junit.runner.RunWith
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
-@RunWith(AndroidJUnit4::class)
+@RunWith(AndroidJUnit4ClassRunner::class)
 class ExampleInstrumentedTest {
     @Test
     fun useAppContext() {
         // Context of the app under test.
-        val appContext = InstrumentationRegistry.getTargetContext()
+        val appContext = ApplicationProvider.getApplicationContext<Context>()
         assertEquals("in.zhiwei.jetpack", appContext.packageName)
     }
 }
