49caeedaf1c9b296e44400c1fbf7e868da4fec8b
志威 <1396188796@qq.com>
2019-09-23 14:46:37 +0800

    format

--- kotlin/src/test/java/org/zhiwei/jetpack/kt/ExampleUnitTest.kt
+++ kotlin/src/test/java/org/zhiwei/jetpack/kt/ExampleUnitTest.kt
@@ -9,37 +9,37 @@ import org.junit.Test
  * @see [Testing documentation](http://d.android.com/tools/testing)
  */
 class ExampleUnitTest {
-	@Test
-	fun addition_isCorrect() {
-		assertEquals(4, (2 + 2).toLong())
-	}
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, (2 + 2).toLong())
+    }
 
-	@Test
-	fun testKt() {
-		println("Byte范围: ${Byte.MIN_VALUE}~${Byte.MAX_VALUE}")
-		println("Short范围: ${Short.MIN_VALUE}~${Short.MAX_VALUE}")
-		println("Int范围: ${Int.MIN_VALUE}~${Int.MAX_VALUE}")
-		println("Long范围: ${Long.MIN_VALUE}~${Long.MAX_VALUE}")
-		println("Float范围: ${Float.MIN_VALUE}~${Float.MAX_VALUE}")
-		println("Double范围: ${Double.MIN_VALUE}~${Double.MAX_VALUE}")
-	}
+    @Test
+    fun testKt() {
+        println("Byte范围: ${Byte.MIN_VALUE}~${Byte.MAX_VALUE}")
+        println("Short范围: ${Short.MIN_VALUE}~${Short.MAX_VALUE}")
+        println("Int范围: ${Int.MIN_VALUE}~${Int.MAX_VALUE}")
+        println("Long范围: ${Long.MIN_VALUE}~${Long.MAX_VALUE}")
+        println("Float范围: ${Float.MIN_VALUE}~${Float.MAX_VALUE}")
+        println("Double范围: ${Double.MIN_VALUE}~${Double.MAX_VALUE}")
+    }
 
-	@Test
-	fun testKtClazz() {
+    @Test
+    fun testKtClazz() {
 //        val tt = KtClazz4("名字")
 //		val (name, age, sex, desc) = User("小明", 22, 1, "小明是个男的，大学生一枚")
-		for (i in 10 until 1) {
-			println(i)
-		}
+        for (i in 10 until 1) {
+            println(i)
+        }
 //        println(tt.toString())
-	}
+    }
 
-	@Test
-	fun testWhile() {
-		var a = 2
-		while (a in 1 until 5) {
-			println(a)
-			a += 2
-		}
-	}
+    @Test
+    fun testWhile() {
+        var a = 2
+        while (a in 1 until 5) {
+            println(a)
+            a += 2
+        }
+    }
 }
