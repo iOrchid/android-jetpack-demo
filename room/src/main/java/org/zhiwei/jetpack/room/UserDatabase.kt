ce8de6aba2f8eb4b5d57be62fc82165265c72cd0
志威 <1396188796@qq.com>
2020-07-26 22:09:44 +0800

    修正room的entity编译时候的export schema

--- room/src/main/java/org/zhiwei/jetpack/room/UserDatabase.kt
+++ room/src/main/java/org/zhiwei/jetpack/room/UserDatabase.kt
@@ -10,7 +10,7 @@ import androidx.room.RoomDatabase
  * Author: zhiwei.
  * Date: 2018/11/5 0005,15:19.
  */
-@Database(entities = [DbUser::class], version = 1)
+@Database(entities = [DbUser::class], version = 1, exportSchema = false)
 abstract class UserDatabase : RoomDatabase() {
 	abstract val userDao: UserDao?
 
