e37c09621165828ab4064de2ab0fcbf534640776
志威 <1396188796@qq.com>
2020-07-26 15:10:49 +0800

    LiveData--2.0--基础的mutableLiveData的使用

---
+++ livedata/src/main/java/org/zhiwei/jetpack/live/BananaFragment.kt
@@ -0,0 +1,42 @@
+package org.zhiwei.jetpack.live
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import kotlinx.android.synthetic.main.fg_banana.*
+
+/**
+ * 作者： 志威  zhiwei.org
+ * 主页： Github: https://github.com/zhiwei1990
+ * 日期： 2020年04月27日 14:51
+ * 签名： 天行健，君子以自强不息；地势坤，君子以厚德载物。
+ *      _              _           _     _   ____  _             _ _
+ *     / \   _ __   __| |_ __ ___ (_) __| | / ___|| |_ _   _  __| (_) ___
+ *    / _ \ | '_ \ / _` | '__/ _ \| |/ _` | \___ \| __| | | |/ _` | |/ _ \
+ *   / ___ \| | | | (_| | | | (_) | | (_| |  ___) | |_| |_| | (_| | | (_) |
+ *  /_/   \_\_| |_|\__,_|_|  \___/|_|\__,_| |____/ \__|\__,_|\__,_|_|\___/  -- 志威 zhiwei.org
+ *
+ * You never know what you can do until you try !
+ * ----------------------------------------------------------------
+ *
+ */
+class BananaFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fg_banana, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        (requireActivity() as LiveActivity).liveAppleData.observe(viewLifecycleOwner, Observer {
+            tv_live_banana.text = it
+        })
+    }
+}
