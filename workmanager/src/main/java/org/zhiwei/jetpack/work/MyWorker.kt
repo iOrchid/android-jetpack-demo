85345bc8e5a378ce5aa42c7a38b3be879c0ad6d9
志威 <1396188796@qq.com>
2020-11-14 13:34:45 +0800

    演示workmanager

--- workmanager/src/main/java/org/zhiwei/jetpack/work/MyWorker.kt
+++ workmanager/src/main/java/org/zhiwei/jetpack/work/MyWorker.kt
@@ -1,29 +1,43 @@
 package org.zhiwei.jetpack.work
 
 import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.util.Log
+import androidx.work.CoroutineWorker
 import androidx.work.Data
-import androidx.work.Worker
 import androidx.work.WorkerParameters
+import com.blankj.utilcode.util.FileUtils
+import com.blankj.utilcode.util.PathUtils
+import kotlinx.coroutines.delay
 
 /**
  * 继承worker，实现doWork，类似于异步任务
  * Author: zhiwei.
  * Date: 2018/11/6 0006,13:52.
  */
-class MyWorker(context: Context, workerParams: WorkerParameters) : Worker(context, workerParams) {
-	//1、创建worker，执行任务的执行者
-	override fun doWork(): Result { //在workRequest中传递来的data，
-		val data = inputData
-		val name = data.getString("name")
-		val age = data.getInt("age", 0)
-		//do some work
-		Log.i("test", "worker doWork() name: $name age: $age")
-		//返回任务的结果数据，
-		val out = Data.Builder()
-			.putString("result", "哈哈哈，真的可以返回呀")
-			.putInt("status", 200)
-			.build()
-		return Result.success(out)
-	}
+class MyWorker(private val context: Context, private val workerParams: WorkerParameters) :
+    CoroutineWorker(context, workerParams) {
+    //1、创建worker，执行任务的执行者
+    override suspend fun doWork(): Result { //在workRequest中传递来的data，
+        val data = inputData
+        val name = data.getString("name")
+        val age = data.getInt("age", 0)
+        //do some work
+        delay(5 * 1000)
+        context.startActivity(Intent(Intent.ACTION_VIEW).also {
+            it.data = Uri.parse("https://www.baidu.com")
+            it.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        })
+        val path = PathUtils.getExternalAppDownloadPath() + "/work.ttt"
+
+        Log.i("test", "worker doWork() name: $name age: $age  $path")
+        FileUtils.createOrExistsFile(path)
+        //返回任务的结果数据，
+        val out = Data.Builder()
+            .putString("result", "哈哈哈，真的可以返回呀")
+            .putInt("status", 200)
+            .build()
+        return Result.success(out)
+    }
 }
--- workmanager/src/main/java/org/zhiwei/jetpack/work/WorkActivity.kt
+++ workmanager/src/main/java/org/zhiwei/jetpack/work/WorkActivity.kt
@@ -6,6 +6,8 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.work.*
+import com.blankj.utilcode.constant.PermissionConstants
+import com.blankj.utilcode.util.PermissionUtils
 import java.util.concurrent.TimeUnit
 
 /**
@@ -24,57 +26,64 @@ import java.util.concurrent.TimeUnit
  * workmanager的演示界面
  */
 class WorkActivity : AppCompatActivity() {
-	//todo 这里workmanager的request有个高级用法，就是添加环境约束 ，比如网络、电量等
-	private var constraints = Constraints.Builder()
-		.setRequiredNetworkType(NetworkType.CONNECTED) //联网状态
-		.setRequiresBatteryNotLow(true) //低电量不操作
-		.setRequiresCharging(true) //充电时候才开始
+    //todo 这里workmanager的request有个高级用法，就是添加环境约束 ，比如网络、电量等
+    private var constraints = Constraints.Builder()
+        .setRequiredNetworkType(NetworkType.CONNECTED) //联网状态
+        .setRequiresBatteryNotLow(true) //低电量不操作
+//        .setRequiresCharging(true) //todo 充电时候才开始
 //            .setRequiresDeviceIdle(true)//待机状态下才执行，api 23 以上
-		.setRequiresStorageNotLow(true) //存储空间不能太小
-		.build()
-	private val name = "zhiwei"
-	private val age = 18
-	//类似于intent的bundle
-	private var data = Data.Builder()
-		.putString("name", name)
-		.putInt("age", age)
-		.build()
-	//2、创建一个workrequest 这里有onetime,还有个PeriodicWorkRequest
-	private val workRequest: WorkRequest = OneTimeWorkRequest.Builder(MyWorker::class.java)
-		.setConstraints(constraints) //添加约束
-		.setInputData(data) //传递data到worker中
-		.build()
-	//todo worker的角色定位用于特殊的任务操作，可以脱离于本App的进程，所以这里的定期任务，做了最小限制，间隔至少15分钟，最小弹性伸缩时间为5分钟
-	private val request: WorkRequest =
-		PeriodicWorkRequest.Builder(MyWorker::class.java, 15, TimeUnit.MINUTES)
-			.setBackoffCriteria(BackoffPolicy.LINEAR, 20, TimeUnit.MINUTES)
-			.build()
+        .setRequiresStorageNotLow(true) //存储空间不能太小
+        .build()
+    private val name = "zhiwei"
+    private val age = 18
 
-	override fun onCreate(savedInstanceState: Bundle?) {
-		super.onCreate(savedInstanceState)
-		setContentView(R.layout.activity_work)
-		//3、加入任务管理，但不是执行，执行的代码稍后
-		WorkManager.getInstance(this).enqueue(workRequest)
-		//4、通过workRequest的唯一标记id，来操作request，并获取返回数据
+    //类似于intent的bundle
+    private var data = Data.Builder()
+        .putString("name", name)
+        .putInt("age", age)
+        .build()
+
+    //2、创建一个workrequest 这里有onetime,还有个PeriodicWorkRequest
+    private val workRequest: WorkRequest = OneTimeWorkRequest.Builder(MyWorker::class.java)
+//        .setConstraints(constraints) //添加约束,todo 这里的条件会限制执行结果，比如上面开启了充电时候才执行的限制
+        .setInputData(data) //传递data到worker中
+        .build()
+
+    //todo worker的角色定位用于特殊的任务操作，可以脱离于本App的进程，所以这里的定期任务，做了最小限制，间隔至少15分钟，最小弹性伸缩时间为5分钟
+    private val request: WorkRequest =
+        PeriodicWorkRequest.Builder(MyWorker::class.java, 15, TimeUnit.MINUTES)
+            .setBackoffCriteria(BackoffPolicy.LINEAR, 20, TimeUnit.MINUTES)
+            .build()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_work)
+        PermissionUtils.permission(PermissionConstants.STORAGE).request()
+        //3、加入任务管理，但不是执行，执行的代码稍后
+        WorkManager.getInstance(this@WorkActivity)
+            .enqueue(workRequest)
+
+        //4、通过workRequest的唯一标记id，来操作request，并获取返回数据
 //todo 这里因为在oncreate中执行，会先与work执行，toast会弹出未执行work的空结果，work变化后，还会显示出成功后的结果。这是因为observe监测worker的status变化 enqueued、RUNNING、successed、retry、failure等
-		WorkManager.getInstance(this).getWorkInfoByIdLiveData(workRequest.id)
-			.observe(this, Observer { workStatus: WorkInfo ->
-				//接收从worker中返回的任务结果,最好在这里判断status为success再做具体操作
-				if (workStatus.state == WorkInfo.State.SUCCEEDED) {
-					val data = workStatus.outputData
-					val result = data.getString("result")
-					val status = data.getInt("status", 0)
-					Toast.makeText(
-						this@WorkActivity,
-						"result: $result status: $status",
-						Toast.LENGTH_SHORT
-					).show()
-					Log.i("WorkActivity", "workStatus: " + workStatus.state.name)
-				}
-			})
-	}
+        WorkManager.getInstance(this).getWorkInfoByIdLiveData(workRequest.id)
+            .observe(this, Observer { workStatus: WorkInfo ->
+                //接收从worker中返回的任务结果,最好在这里判断status为success再做具体操作
+                if (workStatus.state == WorkInfo.State.SUCCEEDED) {
+                    val data = workStatus.outputData
+                    val result = data.getString("result")
+                    val status = data.getInt("status", 0)
+                    Toast.makeText(
+                        this@WorkActivity,
+                        "result: $result status: $status",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    Log.i("WorkActivity", "workStatus: " + workStatus.state.name)
+                }
+            })
 
-	//<editor-folder desc="工作流 演示">
+    }
+
+    //<editor-folder desc="工作流 演示">
 /*{
         OneTimeWorkRequest workA = null, workB = null, workC = null, workD = null, workE = null;
         //串行
@@ -100,8 +109,9 @@ class WorkActivity : AppCompatActivity() {
                 .enqueue();
     }*/
 //</editor-folder>
-	override fun onDestroy() {
-		super.onDestroy()
-		WorkManager.getInstance(this).cancelWorkById(workRequest.id)
-	}
+
+    override fun onDestroy() {
+        super.onDestroy()
+        WorkManager.getInstance(this).cancelWorkById(workRequest.id)
+    }
 }
