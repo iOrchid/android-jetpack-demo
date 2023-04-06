d820be277a0e69f66c81a9f1fd7fb14b24cab830
志威 <1396188796@qq.com>
2023-04-06 15:15:11 +0800

    暂时未用ksp和hilt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,13 @@
+
 plugins {
 	@Suppress("DSL_SCOPE_VIOLATION")// 这属于gradle和IDE产生的bug，不影响运行，但是使用体验不佳
 	alias(libs.plugins.com.android.application)
 	@Suppress("DSL_SCOPE_VIOLATION")
 	alias(libs.plugins.org.jetbrains.kotlin.android)
+	//使用ksp替代kapt 第2步，在模块的build.gradle中添加plugins
+	@Suppress("DSL_SCOPE_VIOLATION")
+	alias(libs.plugins.ksp)
+	id("kotlin-parcelize")
 }
 
 android {
@@ -29,27 +34,82 @@ android {
 		}
 	}
 	compileOptions {
-		sourceCompatibility = JavaVersion.VERSION_1_8
-		targetCompatibility = JavaVersion.VERSION_1_8
+		sourceCompatibility = JavaVersion.VERSION_17
+		targetCompatibility = JavaVersion.VERSION_17
 	}
 	kotlinOptions {
-		jvmTarget = "1.8"
+		jvmTarget = "17"
 	}
 	buildFeatures {
 		viewBinding = true
+	}
+
+	configurations {
+		// room 2.5.1中compiler依赖了老旧的com.intellij的annotations 12的包，与 org.jetbrains.annotations 13的包冲突了
+		implementation.configure { exclude(group = "com.intellij", module = "annotations") }
 	}
 }
 
 dependencies {
 
-	implementation(libs.core.ktx)
+	// android official libs version
+	implementation(libs.activity.ktx)
 	implementation(libs.appcompat)
-	implementation(libs.material)
 	implementation(libs.constraintlayout)
+	implementation(libs.core.ktx)
+	implementation(libs.fragment.ktx)
+	implementation(libs.material)
+	implementation(libs.recyclerview)
+	implementation(libs.swiperefreshlayout)
+
+
+	// androidx 或 google 相关库
+	implementation(libs.androidx.biometric)
+	implementation(libs.androidx.browser)
+	implementation(libs.androidx.camera.camera2)
+	implementation(libs.androidx.core.splashscreen)
+	implementation(libs.androidx.dataStore.core)
+	implementation(libs.androidx.dataStore.preferences)
+	implementation(libs.androidx.drawerlayout)
+	implementation(libs.androidx.emoji2)
+	implementation(libs.androidx.exifinterface)
+	implementation(libs.androidx.metrics)
+	implementation(libs.androidx.preference)
+	implementation(libs.androidx.profileinstaller)
+	implementation(libs.androidx.startup)
+	implementation(libs.androidx.tracing)
+	implementation(libs.androidx.webkit)
+
+	// Jetpack Components libs version
+	implementation(libs.lifecycle.livedata.ktx)
+	implementation(libs.lifecycle.viewmodel.ktx)
 	implementation(libs.navigation.fragment.ktx)
 	implementation(libs.navigation.ui.ktx)
+	implementation(libs.room.compiler)
+	implementation(libs.room.runtime)
+	implementation(libs.room.ktx)
+	implementation(libs.paging.runtime.ktx)
+	implementation(libs.work.runtime.ktx)
 
+	// jetbrains official libs version
+	implementation(libs.kotlin.stdlib)
+	implementation(libs.kotlinx.coroutines.android)
+	implementation(libs.kotlinx.datetime)
+	implementation(libs.kotlinx.serialization.json)
 
+	// 常用知名开源库
+	implementation(libs.blankj.utils)
+	implementation(libs.coil.kt)
+	implementation(libs.coil.kt.svg)
+	implementation(libs.gson)
+	implementation(libs.koin)
+	implementation(libs.okhttp.logging)
+	implementation(libs.retrofit.core)
+	implementation(libs.retrofit.kotlin.serialization)
+	implementation(libs.retrofit2.converter.gson)
+
+	//test libs version
+	testImplementation(libs.turbine)
 	testImplementation(libs.junit)
 	androidTestImplementation(libs.androidx.test.ext.junit)
 	androidTestImplementation(libs.espresso.core)
--- build.gradle.kts
+++ build.gradle.kts
@@ -4,4 +4,8 @@ plugins {
 	alias(libs.plugins.com.android.application) apply false
 	@Suppress("DSL_SCOPE_VIOLATION")
 	alias(libs.plugins.org.jetbrains.kotlin.android) apply false
+	//用于替换kapt的，ksp插件实现注解处理；1，项目build.gradle中添加plugins
+	// 可使用旧的id("com.google.devtools.ksp") version "1.8.10-1.0.9" apply false形式
+	@Suppress("DSL_SCOPE_VIOLATION")//如果不想临时处理这个IDE报错，
+	alias(libs.plugins.ksp) apply false
 }
