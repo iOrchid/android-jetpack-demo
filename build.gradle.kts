ebd954fb528debdc5653d430402427ed3f5b2e9f
iOrchid <zhiwei9001@qq.com>
2024-04-22 20:27:22 +0800

    改一下navigation的safe args插件依赖方式

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,15 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-	alias(libs.plugins.com.android.application) apply false
-	alias(libs.plugins.com.android.library) apply false
-	alias(libs.plugins.org.jetbrains.kotlin.android) apply false
-	//用于替换kapt的，ksp插件实现注解处理；1，项目build.gradle中添加plugins
-	alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.com.android.application) apply false
+    alias(libs.plugins.com.android.library) apply false
+    alias(libs.plugins.org.jetbrains.kotlin.android) apply false
+    //用于替换kapt的，ksp插件实现注解处理；1，项目build.gradle中添加plugins
+    alias(libs.plugins.ksp) apply false
     alias(libs.plugins.androidDynamicFeature) apply false
 }
 //为了navigation的safe args，添加如下方式
 buildscript {
-	dependencies {
-		classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.7.7")
-	}
+    dependencies {
+        classpath(libs.androidx.navigation.safe.args.gradle.plugin)
+    }
 }
