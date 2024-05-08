e92f41f899f4c1874037551e7386ca471e710f2b
iOrchid <zhiwei9001@qq.com>
2024-05-08 16:10:24 +0800

    lookAhead

--- compose/src/main/java/org/zhiwei/compose/screen/layout/OnplaceLayoutId.kt
+++ compose/src/main/java/org/zhiwei/compose/screen/layout/OnplaceLayoutId.kt
@@ -8,8 +8,10 @@ import androidx.compose.animation.core.spring
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
@@ -19,6 +21,7 @@ import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.NotificationsActive
 import androidx.compose.material3.Icon
@@ -26,12 +29,14 @@ import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.movableContentOf
 import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
 import androidx.compose.ui.draw.drawBehind
@@ -39,8 +44,10 @@ import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.layout.LookaheadScope
 import androidx.compose.ui.layout.Measurable
 import androidx.compose.ui.layout.MeasurePolicy
+import androidx.compose.ui.layout.intermediateLayout
 import androidx.compose.ui.layout.layoutId
 import androidx.compose.ui.layout.onPlaced
 import androidx.compose.ui.layout.positionInParent
@@ -63,6 +70,8 @@ internal fun OnPlaceLayoutId_Screen(modifier: Modifier = Modifier) {
         UI_onPlaced()
         UI_LayoutId()
     }
+    //单独演示，可注释掉上面Column部分
+//    UI_LookAheadScope()
 
 }
 
@@ -400,6 +409,107 @@ fun Modifier.drawChecker() = this.then(
         }
     }
 )
+//endregion
+
+
+//region lookaHeadScope
+
+@OptIn(ExperimentalComposeUiApi::class)
+fun Modifier.animatePlacementInScope(lookaheadScope: LookaheadScope) = composed {
+    // Creates an offset animation
+    var offsetAnimation: Animatable<IntOffset, AnimationVector2D>? by mutableStateOf(
+        null
+    )
+    var targetOffset: IntOffset? by mutableStateOf(null)
+
+    this.intermediateLayout { measurable, constraints ->
+        val placeable = measurable.measure(constraints)
+        layout(placeable.width, placeable.height) {
+            // Converts coordinates of the current layout to LookaheadCoordinates
+            val coordinates = coordinates
+            if (coordinates != null) {
+                // Calculates the target offset within the lookaheadScope
+                val target = with(lookaheadScope) {
+                    lookaheadScopeCoordinates
+                        .localLookaheadPositionOf(coordinates)
+                        .round().also { targetOffset = it }
+                }
+
+                // Uses the target offset to start an offset animation
+                if (target != offsetAnimation?.targetValue) {
+                    offsetAnimation?.run {
+                        launch { animateTo(target) }
+                    } ?: Animatable(target, IntOffset.VectorConverter).let {
+                        offsetAnimation = it
+                    }
+                }
+                // Calculates the *current* offset within the given LookaheadScope
+                val placementOffset =
+                    lookaheadScopeCoordinates.localPositionOf(
+                        coordinates,
+                        Offset.Zero
+                    ).round()
+                // Calculates the delta between animated position in scope and current
+                // position in scope, and places the child at the delta offset. This puts
+                // the child layout at the animated position.
+                val (x, y) = requireNotNull(offsetAnimation).run { value - placementOffset }
+                placeable.place(x, y)
+            } else {
+                placeable.place(0, 0)
+            }
+        }
+    }
+}
+
+val colors = listOf(
+    Color(0xffff6f69), Color(0xffffcc5c), Color(0xff264653), Color(0xff2a9d84)
+)
+
+/**
+ * 演示动态切换布局方式，使用LookAheadScope
+ */
+@Preview(showBackground = true)
+@Composable
+private fun UI_LookAheadScope() {
+
+    var isInColumn by remember { mutableStateOf(true) }
+    LookaheadScope {
+        // Creates movable content containing 4 boxes. They will be put either in a [Row] or in a
+        // [Column] depending on the state
+        val items = remember {
+            movableContentOf {
+                colors.forEach { color ->
+                    Box(
+                        Modifier
+                            .padding(15.dp)
+                            .size(100.dp, 80.dp)
+                            .animatePlacementInScope(this)
+                            .background(color, RoundedCornerShape(20))
+                    )
+                }
+            }
+        }
+
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .clickable { isInColumn = !isInColumn }
+        ) {
+            // As the items get moved between Column and Row, their positions in LookaheadScope
+            // will change. The `animatePlacementInScope` modifier created above will
+            // observe that final position change via `localLookaheadPositionOf`, and create
+            // a position animation.
+            if (isInColumn) {
+                Column(Modifier.fillMaxSize()) {
+                    items()
+                }
+            } else {
+                Row { items() }
+            }
+        }
+    }
+}
+
 //endregion
 
 @Preview(showBackground = true, backgroundColor = 0xFFFFFF)
