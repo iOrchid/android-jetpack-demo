c52f66b4a656de7a22ecff3c8565a1aed365f429
iOrchid <zhiwei9001@qq.com>
2024-05-08 15:40:22 +0800

    layoutId

--- compose/src/main/java/org/zhiwei/compose/screen/layout/OnplaceLayoutId.kt
+++ compose/src/main/java/org/zhiwei/compose/screen/layout/OnplaceLayoutId.kt
@@ -5,17 +5,25 @@ import androidx.compose.animation.core.AnimationVector2D
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.VectorConverter
 import androidx.compose.animation.core.spring
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.NotificationsActive
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Slider
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableFloatStateOf
@@ -26,14 +34,24 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.layout.Measurable
+import androidx.compose.ui.layout.MeasurePolicy
+import androidx.compose.ui.layout.layoutId
 import androidx.compose.ui.layout.onPlaced
 import androidx.compose.ui.layout.positionInParent
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Constraints
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.round
 import kotlinx.coroutines.launch
+import org.zhiwei.compose.R
 import org.zhiwei.compose.ui.widget.Title_Desc_Text
 import org.zhiwei.compose.ui.widget.Title_Sub_Text
 import org.zhiwei.compose.ui.widget.Title_Text
@@ -41,14 +59,16 @@ import kotlin.math.roundToInt
 
 @Composable
 internal fun OnPlaceLayoutId_Screen(modifier: Modifier = Modifier) {
-
-    UI_onPlaced(modifier)
+    Column(modifier.fillMaxSize()) {
+        UI_onPlaced()
+        UI_LayoutId()
+    }
 
 }
 
 //region onPlaced
 @Composable
-private fun UI_onPlaced(modifier: Modifier) {
+private fun UI_onPlaced() {
     var alignment by remember {
         mutableStateOf(Alignment.Center)
     }
@@ -82,7 +102,7 @@ private fun UI_onPlaced(modifier: Modifier) {
     }
 
     Column(
-        modifier
+        Modifier
             .fillMaxWidth()
             .aspectRatio(1f)
             .padding(10.dp)
@@ -148,6 +168,238 @@ private fun Modifier.animatePlacement(): Modifier = composed {
             animatable?.let { it.value - targetOffset } ?: IntOffset.Zero
         }
 }
+//endregion
+
+//region layoutId
+
+@Composable
+private fun UI_LayoutId() {
+    Title_Text(title = "layoutId的使用")
+    Title_Sub_Text(title = "在自定义的layout中，根据指定layoutId来确定布局方式")
+    MyLayout(
+        Modifier
+            .fillMaxWidth()
+            .border(2.dp, Color.Green)
+    ) {
+        //这里确定的两个layoutId
+        Text(
+            text = "立夏·春光万象中，夏日初长成",
+            Modifier
+                .layoutId("text")
+                .border(2.dp, Color.Red)
+        )
+        Image(
+            painter = painterResource(id = R.drawable.m005),
+            contentDescription = null,
+            Modifier
+                .layoutId("image")
+                .size(100.dp)
+        )
+    }
+    Spacer(modifier = Modifier.height(20.dp))
+    MyLayout(
+        Modifier
+            .fillMaxWidth()
+            .border(2.dp, Color.Green)
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.m005),
+            contentDescription = null,
+            Modifier
+                .layoutId("image")
+                .size(150.dp)
+        )
+        //调换顺序，也不影响布局方式，因为layoutId在MyLayout中确定了布局方式
+        Text(
+            text = "立夏·春光万象中，夏日初长成",
+            Modifier
+                .layoutId("text")
+                .border(2.dp, Color.Red)
+        )
+    }
+    Spacer(modifier = Modifier.height(20.dp))
+    //指定layoutId的icon设置透明度不同
+    PlaceWithLayerLayout(
+        modifier = Modifier
+            .fillMaxWidth()
+            .drawChecker(),
+        alpha = .4f//都是0.4f的都明度
+    ) {
+        Icon(
+            imageVector = Icons.Default.NotificationsActive,
+            contentDescription = null,
+            modifier = Modifier
+                .background(
+                    Color.Red,
+                    CircleShape
+                )
+                .size(80.dp)
+                .padding(10.dp),
+            tint = Color.White,
+        )
+        Icon(
+            imageVector = Icons.Default.NotificationsActive,
+            contentDescription = null,
+            modifier = Modifier
+                .background(
+                    Color.Red,
+                    CircleShape
+                )
+                .size(80.dp)
+                .padding(10.dp),
+            tint = Color.White,
+        )
+        //指定的layoutId，在布局中，会特别处理
+        Icon(
+            imageVector = Icons.Default.NotificationsActive,
+            contentDescription = null,
+            modifier = Modifier
+                .background(
+                    Color.Red,
+                    CircleShape
+                )
+                .layoutId("full_alpha")
+                .size(80.dp)
+                .padding(10.dp),
+            tint = Color.White,
+        )
+        Icon(
+            imageVector = Icons.Default.NotificationsActive,
+            contentDescription = null,
+            modifier = Modifier
+                .background(
+                    Color.Red,
+                    CircleShape
+                )
+                .size(80.dp)
+                .padding(10.dp),
+            tint = Color.White,
+        )
+    }
+}
+
+
+/**
+ * 自定义的，图片在上文字在下的布局，且总体依据图片控件的宽度约束
+ */
+@Composable
+private fun MyLayout(modifier: Modifier = Modifier, content: @Composable () -> Unit) {
+    Layout(content = content, modifier, measurePolicy = { measurables, constraints ->
+        //演示用的，确定的两个layout的id
+        val imageMeasurable: Measurable = measurables.find { it.layoutId == "image" }!!
+        val textMeasurable: Measurable = measurables.find { it.layoutId == "text" }!!
+        // 图片控件设定minSize，因为如果外部传入modifier用的是fillMax，那么min可能就是宽高屏幕
+        val imagePlaceable = imageMeasurable.measure(constraints.copy(minWidth = 0, minHeight = 0))
+
+        // Limit text width to image width by setting min and max width at image width
+        val textPlaceable = textMeasurable.measure(
+            constraints.copy(
+                minWidth = imagePlaceable.width,
+                maxWidth = imagePlaceable.width
+            )
+        )
+        val width = imagePlaceable.width
+        val imagePlaceableHeight = imagePlaceable.height
+        val height = imagePlaceableHeight + textPlaceable.height
+        //这里确定了两个元素的布局顺序和方式
+        layout(width, height) {
+            imagePlaceable.placeRelative(0, 0)
+            textPlaceable.placeRelative(0, imagePlaceableHeight)
+        }
+    })
+}
+
+/**
+ * 自定义的，指定layoutId的图片，设置透明度
+ */
+@Composable
+private fun PlaceWithLayerLayout(
+    modifier: Modifier = Modifier,
+    alpha: Float = 1f,
+    content: @Composable () -> Unit,
+) {
+    val measurePolicy = MeasurePolicy { measurables, constraints ->
+
+        val fullAlphaIndex = measurables.indexOfFirst {
+            it.layoutId == "full_alpha"
+        }
+        val placeablesWidth = measurables.map { measurable ->
+            measurable.measure(
+                constraints.copy(
+                    minWidth = 0,
+                    maxWidth = Constraints.Infinity,
+                    minHeight = 0,
+                    maxHeight = Constraints.Infinity
+                )
+            )
+        }
+
+        val hasBoundedWidth = constraints.hasBoundedWidth
+        val hasFixedWidth = constraints.hasFixedWidth
+
+        val hasBoundedHeight = constraints.hasBoundedHeight
+        val hasFixedHeight = constraints.hasFixedHeight
+
+        val width =
+            if (hasBoundedWidth && hasFixedWidth) constraints.maxWidth
+            else placeablesWidth.sumOf { it.width }.coerceAtMost(constraints.maxWidth)
+
+        val height =
+            if (hasBoundedHeight && hasFixedHeight) constraints.maxHeight
+            else placeablesWidth.maxOf { it.height }.coerceAtMost(constraints.maxHeight)
+
+
+        var posX = 0
+
+        layout(width, height) {
+            placeablesWidth.forEachIndexed { index, placeable ->
+                placeable.placeRelativeWithLayer(posX, 0) {
+                    if (index == fullAlphaIndex) {
+                        this.alpha = 1f
+                    } else {
+                        this.alpha = alpha
+                    }
+                }
+
+                posX += placeable.width
+            }
+        }
+
+    }
+
+    Layout(
+        modifier = modifier,
+        content = content,
+        measurePolicy = measurePolicy
+    )
+}
+
+/**
+ * 绘制背景格子背景的扩展操作符
+ */
+fun Modifier.drawChecker() = this.then(
+    drawBehind {
+        val width = this.size.width
+        val height = this.size.height
+
+        val checkerWidth = 10.dp.toPx()
+        val checkerHeight = 10.dp.toPx()
+
+        val horizontalSteps = (width / checkerWidth).toInt()
+        val verticalSteps = (height / checkerHeight).toInt()
+
+        for (y in 0..verticalSteps) {
+            for (x in 0..horizontalSteps) {
+                val isGrayTile = ((x + y) % 2 == 1)
+                drawRect(
+                    color = if (isGrayTile) Color.LightGray else Color.White,
+                    topLeft = Offset(x * checkerWidth, y * checkerHeight),
+                    size = Size(checkerWidth, checkerHeight)
+                )
+            }
+        }
+    }
+)
 //endregion
 
 @Preview(showBackground = true, backgroundColor = 0xFFFFFF)
